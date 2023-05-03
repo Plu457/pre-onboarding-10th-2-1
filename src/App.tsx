7f711af2378bd67c200a3f6bfc0d2e6134373bf3
plu457 <ekgus5095@gmail.com>
2023-05-03 15:21:04 +0900

    Feat: 절대 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle";
-import Router from "./routes/router";
+import Router from "routes/router";
 
 const App = () => {
   return (
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
+import Home from "pages/Home";
 
 const Router = () => {
   return (
